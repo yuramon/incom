f7220b367ec9c121879419c90be8ee58d6df1e37
yuramon <yuramon2405@gmail.com>
2019-12-17 02:53:27 +0200

    Create Private::Conversation and Private::Message models - Define associations between User, Private::Conversation   and Private::Message models - Define private_conversations and private_messages tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-
+  has_many :private_messages, class_name: 'Private::Message'
+  has_many :private_conversations,
+            foreign_key: :sender_id,
+            class_name: 'Private::Conversation'
   has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
